037faf990a890adea116b3544a0d91c69276850d
GUO YANKE <hi@guoyk.xyz>
2023-10-25 19:26:10 +0800

    feat: add placeholder command for tok715_voicerecog, tok715_vectorstor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tok715
+
 开发代号: TOK715
+
+## 许可证
+
+TOK715 Developers, MIT License
---
+++ tok715/vectorstor/cli.py
@@ -0,0 +1,9 @@
+def main():
+    """Main entry point for the application script"""
+    print("Call your main application code here")
+    print("You can also edit %s to change the behavior" % __file__)
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
